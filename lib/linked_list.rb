7a40fc146a27d32f53c604b402a6a275bfa931fb
Corey Soreff <csoreff@gmail.com>
2015-08-25 13:37:19 -0400

    Created node class and methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,5 +2,25 @@ require 'pry'
 require_relative 'node'
 
 class LinkedList
- 
+  def initialize
+    @head = nil
+  end
+
+  def prepend
+  end
+
+  def each
+  end
+
+  def to_s
+  end
+
+  def [](index)
+  end
+
+  def insert
+  end
+
+  def remove
+  end
 end
