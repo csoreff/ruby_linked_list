d91179e07b73ae7f0a20c4db1abec23612c2a2cd
Corey Soreff <csoreff@gmail.com>
2015-08-25 14:06:17 -0400

    Created initialize, prepend, each, index, and to_s methods for linked list class

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,16 +6,36 @@ class LinkedList
     @head = nil
   end
 
-  def prepend
+  def prepend(info)
+    @head = Node.new(info, @head)
   end
 
   def each
+    node = @head
+
+    while !node.nil?
+      yield node.info
+      node = node.next_node
+    end
   end
 
   def to_s
+    string = []
+    self.each { |node| string << node }
+    string = string.join(", ")
+    "LinkedList(#{string})"
   end
 
   def [](index)
+    index_counter = 0
+    node = @head
+
+    until index_counter == index
+      node = node.next_node unless node.nil?
+      index_counter += 1
+    end
+
+    node.nil? ? nil : node
   end
 
   def insert
--- lib/node.rb
+++ lib/node.rb
@@ -1,8 +1,8 @@
 class Node
   attr_accessor :info, :next_node
 
-  def initialize(data, next_node=nil)
-    self.info = data
+  def initialize(info, next_node=nil)
+    self.info = info
     self.next_node = next_node
   end
 end
